ca2db61731df0187efd35b9c2a172d09434a87ed
fatihsahin291 <musabsahin291@gmail.com>
2023-12-03 16:34:19 +0300

    implemented react query and date-fns packages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import {
 	Route,
 	Routes,
 } from "react-router-dom";
+import {
+	QueryClient,
+	QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import GlobalStyles from "./styles/GlobalStyles";
 
@@ -17,9 +22,19 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			// staleTime: 1000 * 60, // 1 minutes
+			staleTime: 0,
+		},
+	},
+});
+
 function App() {
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
@@ -68,7 +83,7 @@ function App() {
 					/>
 				</Routes>
 			</BrowserRouter>
-		</>
+		</QueryClientProvider>
 	);
 }
 
